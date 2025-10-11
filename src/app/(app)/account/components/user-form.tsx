797d75558ab9880bd776770685ca36d38905903c
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-11 15:54:43 +0330

    update birthday field

--- src/app/(app)/account/components/user-form.tsx
+++ src/app/(app)/account/components/user-form.tsx
@@ -1,16 +1,21 @@
 'use client'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { VisuallyHidden } from 'radix-ui'
 import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Label } from '@/components/ui/label'
 import { Spinner } from '@/components/ui/spinner'
 import { TextField } from '@/components/ui/text-field'
 import { client } from '@/rpc/orpc.client'
@@ -56,38 +61,53 @@ export function UserForm(props: { initialValues: UpdateUserFormSchema }) {
         error={errors.phone?.message}
         {...register('phone')}
       />
-      <Controller
-        name="birthday"
-        control={control}
-        render={({ field }) => (
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <TextField
-                label="تاریخ تولد"
-                readOnly
-                inputClass="text-right"
-                value={new Intl.DateTimeFormat('fa-IR', {
-                  year: 'numeric',
-                  month: '2-digit',
-                  day: '2-digit',
-                }).format(field.value)}
-              />
-            </PopoverTrigger>
-            <PopoverContent className="flex w-auto items-center justify-center border-0 p-2">
-              <Calendar
-                mode="single"
-                selected={field.value}
-                captionLayout="dropdown"
-                onSelect={(e) => {
-                  if (!e) return
-                  setOpen(false)
-                  setValue('birthday', e)
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        )}
-      />
+      <div className="grid gap-2">
+        <Label>تاریخ تولد</Label>
+        <Controller
+          name="birthday"
+          control={control}
+          render={({ field }) => (
+            <Drawer open={open} onOpenChange={setOpen}>
+              <DrawerTrigger asChild>
+                <Button
+                  variant={'outline'}
+                  className="justify-start bg-card shadow-none active:scale-100"
+                >
+                  {new Intl.DateTimeFormat('fa-IR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  }).format(field.value)}
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <VisuallyHidden.Root>
+                  <DrawerHeader>
+                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                    <DrawerDescription>
+                      This action cannot be undone.
+                    </DrawerDescription>
+                  </DrawerHeader>
+                </VisuallyHidden.Root>
+                <div className="flex items-center justify-center border-0">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    captionLayout="dropdown"
+                    className="bg-card"
+                    classNames={{ root: 'w-full' }}
+                    onSelect={(e) => {
+                      if (!e) return
+                      setOpen(false)
+                      setValue('birthday', e)
+                    }}
+                  />
+                </div>
+              </DrawerContent>
+            </Drawer>
+          )}
+        />
+      </div>
       <Button type="submit" disabled={isPending}>
         {isPending && <Spinner />}
         ذخیره
