84587945f0c5c08a743711de28b2e85a94cf5e08
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-12 02:35:19 +0330

    update donate label

--- src/components/common/donate-link.tsx
+++ src/components/common/donate-link.tsx
@@ -3,12 +3,12 @@ import { StarsPattern } from '@/assets/icons/stars'
 import { Label } from '../ui/label'
 
 export const DonateLink = () => (
-  <div className="relative grid gap-2">
-    <Label>لینک حمایت مالی</Label>
+  <div className="relative grid gap-3">
+    <Label>🍵 لینک حمایت مالی</Label>
     <Link
       href="https://daramet.com/soly"
       target="_blank"
-      className="relative flex w-full items-center justify-center overflow-hidden rounded-full bg-primary p-2 text-primary-foreground text-sm shadow-md dark:bg-blue-600"
+      className="relative flex w-full items-center justify-center overflow-hidden rounded-full bg-primary p-2 text-primary-foreground text-sm shadow-md transition-all hover:scale-105 active:scale-90 dark:bg-blue-600"
     >
       <StarsPattern className="pointer-events-none absolute inset-0 size-full opacity-10" />
       <span className="relative z-10 w-full rounded-[inherit] bg-white/15 p-3 text-center dark:text-white">
