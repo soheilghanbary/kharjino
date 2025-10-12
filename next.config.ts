6f5f21cfe569ff388e349145fbed842a3c90733e
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-12 23:35:43 +0330

    update next to canary

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
   env: {
     LIARA_SECRET_KEY: process.env.LIARA_SECRET_KEY,
     LIARA_ACCESS_KEY: process.env.LIARA_ACCESS_KEY,
