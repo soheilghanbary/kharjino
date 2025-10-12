cabbdf7f313c1eda7026e345e2b40139379ef624
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-12 23:58:39 +0330

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
-    serverComponentsHmrCache: true,
   },
   env: {
     LIARA_SECRET_KEY: process.env.LIARA_SECRET_KEY,
