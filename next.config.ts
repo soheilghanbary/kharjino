fb68e7e1b2de3f1204b1051259f3f9b4d1cce761
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-23 13:44:10 +0330

    upgrade to next 16

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
+  cacheComponents: true,
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
