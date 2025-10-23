58d5055070b2bed1152e94b07bfe9002d2b3e1f7
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-23 13:57:53 +0330

    remove cacheComponent

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  cacheComponents: true,
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
