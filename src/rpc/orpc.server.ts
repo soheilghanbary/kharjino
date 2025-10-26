08a27f2fdfd4bc9ce0ab48e98132aa4ab635b0db
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-10-27 01:25:16 +0330

    remove DedupeRequestsPlugin from orpc server

--- src/rpc/orpc.server.ts
+++ src/rpc/orpc.server.ts
@@ -1,7 +1,6 @@
 import 'server-only'
 import { createORPCClient } from '@orpc/client'
 import { RPCLink } from '@orpc/client/fetch'
-import { DedupeRequestsPlugin } from '@orpc/client/plugins'
 import type { RouterClient } from '@orpc/server'
 import { headers } from 'next/headers'
 import { handler } from '@/app/api/rpc/[[...rest]]/route'
@@ -10,16 +9,16 @@ import type { router } from './router'
 const link = new RPCLink({
   url: process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000',
   // method: inferRPCMethodFromRouter(router),
-  plugins: [
-    new DedupeRequestsPlugin({
-      groups: [
-        {
-          condition: () => true,
-          context: {},
-        },
-      ],
-    }),
-  ],
+  // plugins: [
+  //   new DedupeRequestsPlugin({
+  //     groups: [
+  //       {
+  //         condition: () => true,
+  //         context: {},
+  //       },
+  //     ],
+  //   }),
+  // ],
   fetch: async (request) => {
     const { response } = await handler.handle(request, {
       context: {
