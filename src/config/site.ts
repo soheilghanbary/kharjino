4b38c854efef56245730334910c140c3aa2547b9
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-11-04 18:24:33 +0330

    upgrade version

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,7 +7,7 @@ import walletAnimation from '@/assets/lotties/wallet.json'
 export const siteConfig = {
   title: 'Kharjino',
   description: 'Modern web, simplified for Web Developers',
-  v: '2.0',
+  v: '2.1',
 }
 
 export const wizardData = [
