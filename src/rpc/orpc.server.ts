66baf447195d0a3325d78312bc55cc6729a66430
Soheil Ghanbary <soli.ghanbary@gmail.com>
2025-11-16 23:28:15 +0330

    update orpc server

--- src/rpc/orpc.server.ts
+++ src/rpc/orpc.server.ts
@@ -2,13 +2,14 @@ import 'server-only'
 import { createORPCClient } from '@orpc/client'
 import { RPCLink } from '@orpc/client/fetch'
 import { DedupeRequestsPlugin } from '@orpc/client/plugins'
-import type { RouterClient } from '@orpc/server'
+import { inferRPCMethodFromRouter, type RouterClient } from '@orpc/server'
 import { headers } from 'next/headers'
 import { handler } from '@/app/api/rpc/[[...rest]]/route'
-import type { router } from './router'
+import { router } from './router'
 
 const link = new RPCLink({
   url: process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000',
+  method: inferRPCMethodFromRouter(router),
   plugins: [
     new DedupeRequestsPlugin({
       groups: [
